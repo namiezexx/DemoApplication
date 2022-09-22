89806feaae09f1fdd4ca8f1bb5cf7b2591385b26
Lee <jaesukl@c889f3bbbff5.ant.amazon.com>
2022-09-22 22:42:36 +0900

    change

--- src/main/java/com/springfield/demo/controller/MapController.java
+++ src/main/java/com/springfield/demo/controller/MapController.java
@@ -8,9 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -20,11 +17,11 @@ public class MapController {
     private TransportRepository transportRepository;
 
     @GetMapping("map")
-    public String map(@RequestParam(value = "partitionid", required = false) String partitionid, Model model) {
+    public String map(@RequestParam(required = false) Long partitionid, Model model) {
 
         System.out.println(partitionid);
 
-        Optional<Transport> transport = transportRepository.findById(Long.parseLong(partitionid));
+        Optional<Transport> transport = transportRepository.findById(partitionid);
 
         model.addAttribute("latitude", transport.get().getLatitude());
         model.addAttribute("longitude", transport.get().getLongitude());
