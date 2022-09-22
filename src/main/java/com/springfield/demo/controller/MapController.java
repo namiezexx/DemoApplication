ff001cac662e170589da34145e972726cf0040a4
Lee <jaesukl@c889f3bbbff5.ant.amazon.com>
2022-09-22 17:30:18 +0900

    html a tag change

--- src/main/java/com/springfield/demo/controller/MapController.java
+++ src/main/java/com/springfield/demo/controller/MapController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -18,8 +20,13 @@ public class MapController {
     private TransportRepository transportRepository;
 
     @GetMapping("map")
-    public String map(Model model) {
-        Optional<Transport> transport = transportRepository.findById(360L);
+    public String map(Model model, @RequestParam(value = "partitionid", required = false)Long partitionid) {
+
+        if(Objects.isNull(partitionid)) {
+            partitionid = 146L;
+        }
+
+        Optional<Transport> transport = transportRepository.findById(partitionid);
 
         model.addAttribute("latitude", transport.get().getLatitude());
         model.addAttribute("longitude", transport.get().getLongitude());
